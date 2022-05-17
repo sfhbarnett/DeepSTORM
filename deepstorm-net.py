0f6365d00be4fad455fa1d96a91b02468a2013d3
sbarnett <sfhbarnett91@gmail.com>
2022-05-17 22:49:05 +0200

    Create the initial net

---
+++ deepstorm-net.py
@@ -0,0 +1,52 @@
+import torch.nn as nn
+
+class DeepSTORM(nn.Module):
+    def __init__(self):
+        super(DeepSTORM, self).__init__()
+        self.down1 = Down(1,32)
+        self.down2 = Down(32, 64)
+        self.down3 = Down(64, 128)
+        self.midsection = nn.Conv2d(128, 512, kernel_size=3, padding=1)
+        self.up1 = Up(512,128)
+        self.up2 = Up(128,64)
+        self.up3 = Up(64,32)
+        self.up4 = nn.Conv2d(32,1,kernel_size=3,padding=1)
+
+    def forward(self,x):
+        down = self.down1(x)
+        down = self.down2(x)
+        down = self.down3(x)
+        mid = self.midsection(down)
+        up = self.up1(mid)
+        up = self.up2(up)
+        up = self.up3(up)
+        up = self.up4(up)
+        return up
+
+
+
+class Down(nn.Module):
+    def __init__(self, inchannels, outchannels):
+        super(Down, self).__init__()
+        self.level = nn.Sequential(
+            nn.Conv2d(inchannels, outchannels, kernel_size=3, padding=1,bias=False),
+            nn.BatchNorm2d(outchannels),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2)
+        )
+
+    def forward(self, x):
+        x = self.level(x)
+        return x
+
+class Up(nn.Module):
+    def __init__(self, inchannels, outchannels):
+        super(Up, self).__init__()
+        #self.up = nn.ConvTranspose2d(inchannels, inchannels//2, stride=2, kernel_size=2)
+        self.up = nn.Upsample(scale_factor=2)
+        self.conv = nn.Conv2d(inchannels, outchannels)
+
+    def forward(self,x):
+        x = self.up(x)
+        x = self.conv(x)
+        return x
