5eab9ac3d2f9bee6e6da9ec5d33ce7182a73dd42
sbarnett <sfhbarnett91@gmail.com>
2022-05-22 11:43:02 +0200

    added comments

--- generateTrainingData.py
+++ generateTrainingData.py
@@ -8,6 +8,8 @@ from PIL import Image
 from random import randint
 import matplotlib.pyplot as plt
 
+# This is a conversion of the data generation script in the original DEEPSTORM repository from matlab to python
+
 tiffpath = 'DeepSTORM dataset_v1/BIN4 - Training dataset/SimulatedDataset.tif'
 csvpath = 'DeepSTORM dataset_v1/BIN4 - Training dataset/SimulatedDataset.csv'
 outdirectory = 'augments/'
@@ -16,11 +18,11 @@ upsamplingfactor = 8
 
 camerapixelsize = 160
 
-gaussian_sigma = 1
+gaussian_sigma = 1 # gaussian sigma for blurring the spike image
 pathsize = 26*upsamplingfactor
-numpatches = 500
-maxexamples = 10_000
-tiffstack = TiffStack(tiffpath)
+numpatches = 500 # number of cropped sections per thunderstorm image
+maxexamples = 10_000 # max number of examples
+tiffstack = TiffStack(tiffpath) # make containiner for tif data
 
 highres_width = tiffstack.width*upsamplingfactor
 highres_height = tiffstack.height*upsamplingfactor
@@ -29,10 +31,11 @@ highres_patchsize = camerapixelsize/upsamplingfactor
 
 ntrain = min([tiffstack.nfiles*numpatches, maxexamples])
 
-patches = np.zeros((pathsize,pathsize,ntrain))
-heatmaps = np.zeros((pathsize,pathsize,ntrain))
-spikes = np.zeros((pathsize,pathsize,ntrain))
+#patches = np.zeros((pathsize,pathsize,ntrain)) # Not needed since we save the image output directly
+#heatmaps = np.zeros((pathsize,pathsize,ntrain))
+#spikes = np.zeros((pathsize,pathsize,ntrain))
 
+# Read in molecule positions
 localisations = []
 with open(csvpath,'r') as f:
     lines = f.readlines()
@@ -46,7 +49,7 @@ k = 0
 for index in range(1, tiffstack.nfiles):
     img = tiffstack.getimage(index)
     newsize = (tiffstack.width*upsamplingfactor,tiffstack.height*upsamplingfactor)
-    largeimg = np.array(Image.fromarray(img).resize(newsize, Image.Resampling.NEAREST))
+    largeimg = np.array(Image.fromarray(img).resize(newsize, Image.Resampling.NEAREST)) # Upsample image
 
     points = np.asarray([x for x in localisations if int(x[1]) == index+1])
     xpos = np.round(points[:, 2] / highres_patchsize)
@@ -55,10 +58,6 @@ for index in range(1, tiffstack.nfiles):
     spike_images = np.zeros((highres_width+1, highres_height+1))
     spike_images[ypos.astype(int),xpos.astype(int)] = 1
     spike_images = gaussian_filter(spike_images,gaussian_sigma)
-    #plt.imshow(spike_images)
-    # plt.figure()
-    # plt.imshow(img)
-    #plt.show()
 
     if k > ntrain:
         break
@@ -70,9 +69,6 @@ for index in range(1, tiffstack.nfiles):
             spike = spike_images[xcrop:xcrop+208,ycrop:ycrop+208]
             tf.imwrite(os.path.join(outdirectory, 'img/'+str(k)+'.tif'),patch)
             tf.imwrite(os.path.join(outdirectory, 'spikes/'+str(k)+'.tif'), spike)
-
-            #patches[:, :, k] = largeimg[xcrop:xcrop+208,ycrop:ycrop+208]
-            #heatmaps[:, :, k] = spike_images[xcrop:xcrop+208,ycrop:ycrop+208]
 
             #scale1 = patches[:, :, k]/np.max(patches[:, :, k])*255
             #scale2 = heatmaps[:, :, k]/np.max(heatmaps[:, :, k])*255
